1d269247001efab3548cec0bee8850900b889e72
3lchicharit0 <88722345+3lchicharit0@users.noreply.github.com>
2021-09-16 09:04:48 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+function feliz () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+}
+function enojado () {
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        # . . . #
+        . # # # .
+        . # . # .
+        `)
+}
+function triste () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # # # .
+        # . . . #
+        `)
+}
+function por_que () {
+    basic.showLeds(`
+        . # . . .
+        # . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+}
+function eeeeeee () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        # # # # #
+        . . . . .
+        `)
+}
